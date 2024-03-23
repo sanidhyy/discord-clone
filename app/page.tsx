0cce9be93fe234e7cff8d39e65b833f7b8b1668e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-23 18:25:31 +0000

    shadcn-ui btn test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="h-full flex flex-col gap-y-4 items-center justify-center">
       <p className="text-7xl font-bold text-indigo-500">Home Page</p>
+      <Button variant="destructive" size="lg">
+        Click me
+      </Button>
     </div>
   );
 };
