0f3609962d358c83c3a4dec862a7840e5638403f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-23 18:36:24 +0000

    clerk provider setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 
@@ -16,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
