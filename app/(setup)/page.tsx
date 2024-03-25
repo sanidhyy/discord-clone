eadfe35245f14be21f3d3173b4f8ba6d884e604e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-25 13:55:54 +0000

    initial profile util created

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) redirect(`/servers/${server.id}`);
+
+  return <div>Create a Server</div>;
+};
+
+export default SetupPage;
