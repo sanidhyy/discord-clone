07aea04fe43253cce19f7bd71b15d18a7693f6fe
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-26 07:10:10 +0000

    fetch current profile util created

---
+++ lib/current-profile.ts
@@ -0,0 +1,17 @@
+import { auth } from "@clerk/nextjs";
+
+import { db } from "./db";
+
+export const currentProfile = async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  return profile;
+};
