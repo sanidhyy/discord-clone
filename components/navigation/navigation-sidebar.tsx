347478c86b79c3c59e1c52fddbb14ee012b3ab77
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-26 07:54:28 +0000

    typo fix

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -29,6 +29,7 @@ export const NavigationSidebar = async () => {
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+
       <ScrollArea className="flex-1 w-full">
         {servers.map((server) => (
           <div key={server.id} className="mb-4">
