ce0b50905dc8d6d5749d8fdac36362c297089986
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-26 07:53:36 +0000

    custom clerk user button created

---
+++ components/clerk/user-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { UserButton as ClerkUserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
+
+export const UserButton = () => {
+  const { theme } = useTheme();
+
+  return (
+    <ClerkUserButton
+      appearance={{
+        baseTheme: theme === "dark" ? dark : undefined,
+        elements: {
+          avatarBox: "h-[48px] w-[48px]",
+        },
+      }}
+    />
+  );
+};
