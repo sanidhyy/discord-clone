6375351e77864590930e4d9cffc46106b018827f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-26 07:54:13 +0000

    navigation sidebar completed

---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+
+import { ActionTooltip } from "@/components/action-tooltip";
+import { cn } from "@/lib/utils";
+
+type NavigationItemProps = {
+  id: string;
+  imageUrl: string;
+  name: string;
+};
+
+export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/servers/${id}`);
+  };
+
+  return (
+    <ActionTooltip side="right" align="center" label={name}>
+      <button onClick={onClick} className="group relative flex items-center">
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]"
+          )}
+          aria-hidden
+        />
+
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId === id &&
+              "bg-primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image src={imageUrl} alt={name} fill />
+        </div>
+      </button>
+    </ActionTooltip>
+  );
+};
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,9 +1,14 @@
 import { redirect } from "next/navigation";
 
+import { UserButton } from "@/components/clerk/user-button";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 import { NavigationAction } from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -23,6 +28,23 @@ export const NavigationSidebar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton />
+      </div>
     </div>
   );
 };
