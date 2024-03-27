829dd49e717a0822f5a8070c0e6342dc8a39b157
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-27 06:49:01 +0000

    edit server api route implemented

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,31 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
+
+    if (!profile) return new NextResponse("Unauthorized.", { status: 401 });
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error: unknown) {
+    console.error("[SERVER_ID_PATCH]: ", error);
+    return new NextResponse("Internal Server Error.", { status: 500 });
+  }
+}
