c954d33ba6a36330dd54aa46323c4c8728ceb711
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-27 08:29:50 +0000

    leave server api route implemented

---
+++ app/api/servers/[serverId]/leave/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  _req: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      serverId: string;
+    };
+  }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) return new NextResponse("Unauthorized.", { status: 401 });
+
+    if (!params.serverId)
+      return new NextResponse("Server ID is missing.", { status: 400 });
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: {
+          not: profile.id,
+        },
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      data: {
+        members: {
+          deleteMany: {
+            profileId: profile.id,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error: unknown) {
+    console.error("[SERVER_ID]: ", error);
+    return new NextResponse("Internal Server Error.", { status: 500 });
+  }
+}
