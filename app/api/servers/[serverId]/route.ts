88907d112a514b5689f49a2e7fdb84582889f18a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-27 08:36:51 +0000

    delete server route implemented

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -29,3 +29,26 @@ export async function PATCH(
     return new NextResponse("Internal Server Error.", { status: 500 });
   }
 }
+
+export async function DELETE(
+  _req: NextRequest,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) return new NextResponse("Unauthorized.", { status: 401 });
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error: unknown) {
+    console.error("[SERVER_ID_DELETE]: ", error);
+    return new NextResponse("Internal Server Error.", { status: 500 });
+  }
+}
