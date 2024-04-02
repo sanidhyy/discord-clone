0ea2f85cb4c06651d8b7224b4fcc961e6fa12898
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-02 14:22:05 +0000

    edit channel modal ui created

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,7 @@ import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
@@ -28,6 +29,7 @@ export const ModalProvider = () => {
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
