9f581344b6fbe5fb486eae637dec79bd5521ceda
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-02 14:54:23 +0000

    onclick handlers added

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -32,8 +32,13 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
 
   const icon = roleIconMap[member.role];
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
+
   return (
     <button
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
