6ec9bf931f7afecb558aa167eb91190eed429aa5
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-02 16:22:01 +0000

    conversation util implemented

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 
 import { MobileToggle } from "../mobile-toggle";
+import { UserAvatar } from "../user-avatar";
 
 type ChatHeaderProps = {
   serverId: string;
@@ -21,6 +22,14 @@ export const ChatHeader = ({
 
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+
+      {type === "conversation" && (
+        <UserAvatar
+          src={imageUrl}
+          alt={name}
+          className="h-8 w-8 md:h-8 md:w-8 mr-2"
+        />
       )}
 
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
