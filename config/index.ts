e486f54b49c05e58e5e33c828230d74bbea0b7d8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-03 17:39:00 +0000

    site config added

---
+++ config/index.ts
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Discord Clone",
+  description:
+    "Real-time chat, voice, and community features in a modern, scalable Next.js 14-powered platform.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "liveblocks",
+    "livekit",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "discord-clone",
+    "realtime-chat",
+    "live-audio",
+    "live-video",
+    "live-chat",
+    "neon-db",
+    "sonner",
+    "zustand",
+    "zod",
+    "sql",
+    "postgresql",
+    "aiven",
+    "lucide-react",
+    "next-themes",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Sanidhya Kumar Verma",
+    url: "https://github.com/sanidhyy",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/sanidhyy/discord-clone",
+} as const;
