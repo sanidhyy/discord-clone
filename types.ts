8f8e52486e0bf1d496fdf8c8db58b04f48dbe000
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-03 15:05:26 +0000

    message types added

--- types.ts
+++ types.ts
@@ -1,10 +1,16 @@
-import type { Member, Profile, Server } from "@prisma/client";
+import type { Member, Message, Profile, Server } from "@prisma/client";
 import type { Server as NetServer, Socket } from "net";
 import type { NextApiResponse } from "next";
 import type { Server as SocketIOServer } from "socket.io";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
+};
+
+export type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile;
+  };
 };
 
 export type NextApiResponseServerIo = NextApiResponse & {
